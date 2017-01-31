8905da195828a9f3bd9cea3fbc9ae49c523cfe54
BBK-PiJ-2016-28 <urahara.nobu@gmail.com>
2017-01-31 00:30:33 +0000

    Tests - this is the first round of testing for ContactManagerImpl

---
+++ Test/ContactManagerTest.java
@@ -0,0 +1,26 @@
+package Test;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+import Impl.ContactImpl;
+import Impl.ContactManagerImpl;
+
+
+public class ContactManagerTest {
+	
+
+	public static void main(String[] args) {
+		int year = 2017;
+		int month = 03;
+		int day = 27;
+		Calendar date = Calendar.getInstance();
+		date.set(year, month, day);
+		ContactImpl mark = new ContactImpl();
+		Set cons = new HashSet<ContactImpl>();
+		cons.add(mark);
+		ContactManagerImpl cm = new ContactManagerImpl();
+		cm.addFutureMeeting(cons, date);
+
+	}
+
+}
